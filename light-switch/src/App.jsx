bf12b0ceb236244482a2ed61eaae5f214266c0f4
Esimehro <loisomodibo@gmail.com>
2023-05-17 02:10:43 +0100

    light switch built with react

---
+++ light-switch/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import DarkModeApp from './Components/DarkModeApp'
+import './index.css'
+
+
+function App() {
+
+  return (
+    <>
+      <DarkModeApp/>
+    </>
+  )
+}
+
+export default App
